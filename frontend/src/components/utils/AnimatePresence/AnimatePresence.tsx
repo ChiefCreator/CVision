4f961a99cbd7936027b40d424a8191d2d68bba25
Kirill Pekarskiy <kipe122006@gmail.com>
2025-08-02 20:54:51 +0300

    feat: create animate presence component

---
+++ frontend/src/components/utils/AnimatePresence/AnimatePresence.tsx
@@ -0,0 +1,63 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+
+import type { BaseComponent } from '@/types/root';
+
+export interface AnimatePresenceProps extends BaseComponent {
+  children: React.ReactNode;
+  isVisible: boolean;
+  enter?: gsap.TweenVars;
+  exit?: gsap.TweenVars;
+  styles?: React.CSSProperties;
+  onHidden?: () => void;
+}
+
+export default function AnimatePresence({ children, isVisible, enter, exit, styles, className, onHidden }: AnimatePresenceProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [shouldRender, setShouldRender] = useState(isVisible);
+
+  const defaultEnter: gsap.TweenVars = {
+    opacity: 1,
+    duration: .4,
+    ease: "none",
+  };
+  const defaultExit: gsap.TweenVars = {
+    opacity: 0,
+    duration: .4,
+    ease: "none",
+  };
+
+  useLayoutEffect(() => {
+    if (isVisible) setShouldRender(true);
+  }, [isVisible]);
+
+  // Анимация входа
+  useLayoutEffect(() => {
+    if (shouldRender && isVisible && ref.current) {
+      gsap.fromTo(
+        ref.current,
+        exit || defaultExit,
+        enter || defaultEnter
+      );
+    }
+  }, [shouldRender, isVisible]);
+
+  // Анимация выхода
+  useLayoutEffect(() => {
+    if (!isVisible && ref.current) {
+      gsap.to(ref.current, {
+        ...(exit || defaultExit),
+        onComplete: () => {
+          setShouldRender(false);
+          onHidden?.();
+        },
+      });
+    }
+  }, [isVisible]);
+
+  if (!shouldRender) return null;
+
+  return (
+    <div ref={ref} style={styles} className={className}>{children}</div>
+  );
+};
---
+++ frontend/src/components/utils/AnimatePresence/components/AnimateHeightPresence.tsx
@@ -0,0 +1,24 @@
+import AnimatePresence, { AnimatePresenceProps } from "../AnimatePresence";
+
+interface AnimateHeightPresenceProps extends Omit<AnimatePresenceProps, "children" | "enter" | "exit"> {
+  children: React.ReactNode;
+}
+
+export default function AnimateHeightPresence({ children, ...restProps }: AnimateHeightPresenceProps) {
+  const enter: gsap.TweenVars = {
+    height: "auto",
+    duration: .4,
+    ease: "power2.inOut",
+  };
+  const exit: gsap.TweenVars = {
+    height: 0,
+    duration: .4,
+    ease: "power2.inOut",
+  };
+
+  return (
+    <AnimatePresence {...restProps} enter={enter} exit={exit} styles={{ overflow: "hidden" }}>
+      {children}
+    </AnimatePresence>
+  );
+}
