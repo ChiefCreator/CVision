a58b761d809635d0b5f7ffca69785d3ea1e39db0
Pekarski <kiryl.pekarski.dev@gmail.com>
2025-08-24 13:45:38 +0300

    feat: create user service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import { DocumentModule } from './document/document.module';
 import { ResumeModule } from './resume/resume.module';
 import { SectionResumeModule } from './section-resume/section-resume.module';
 import { SubsectionResumeModule } from './subsection-resume/subsection-resume.module';
-import { DocumentModule } from './document/document.module';
-import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
-import databaseConfig from './config/database.config';
 import appConfig from './config/app.config';
 import cookieConfig from './config/cookie.config';
-import sessionConfig from './config/session.config';
+import databaseConfig from './config/database.config';
 import redisConfig from './config/redis.config';
+import sessionConfig from './config/session.config';
 
 import { isProd } from './utils/env.utils';
 
@@ -20,13 +21,14 @@ import { isProd } from './utils/env.utils';
     SectionResumeModule,
     SubsectionResumeModule,
     DocumentModule,
+    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
       ignoreEnvFile: isProd(),
       envFilePath: ".env",
       cache: true,
       load: [appConfig, databaseConfig, cookieConfig, sessionConfig, redisConfig]
-    })
+    }),
   ],
 })
 export class AppModule {}
