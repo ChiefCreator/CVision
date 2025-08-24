26d8f3e9a703807b72751e5fc87aecac4d3f02bb
Pekarski <kiryl.pekarski.dev@gmail.com>
2025-08-24 17:33:17 +0300

    fix: change receive session max age

--- backend/src/config/session.config.ts
+++ backend/src/config/session.config.ts
@@ -3,8 +3,8 @@ import { registerAs } from '@nestjs/config';
 export default registerAs("session", () => ({
   secret: process.env.SESSION_SECRET,
   name: process.env.SESSION_NAME ?? "sid",
-  domain: process.env.SESSION_DOMAIN ?? "sid",
-  maxAge: parseInt(process.env.SESSION_MAX_AGE ?? "86400000"),
+  domain: process.env.SESSION_DOMAIN,
+  maxAge: process.env.SESSION_MAX_AGE ?? "86400000",
   httpOnly: process.env.SESSION_HTTP_ONLY === "true",
   secure: process.env.SESSION_SECURE === "true",
   sameSite: (process.env.SESSION_SAME_SITE ?? "lax") as "lax" | "strict" | "none",
