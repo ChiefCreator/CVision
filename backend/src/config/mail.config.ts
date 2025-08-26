bdd37be2ab61329ef62f9cd31fb5938c502d929d
Pekarski <kiryl.pekarski.dev@gmail.com>
2025-08-26 22:45:11 +0300

    fix: change port name in mail config

--- backend/src/config/mail.config.ts
+++ backend/src/config/mail.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("mail", () => ({
 	host: process.env.MAIL_HOST,
-	post: Number(process.env.MAIL_PORT || 587),
+	port: Number(process.env.MAIL_PORT || 587),
 	login: process.env.MAIL_LOGIN,
 	password: process.env.MAIL_PASSWORD,
 }))
