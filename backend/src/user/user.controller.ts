e2bd32318a1b44e7e9b991925e66418db70ba9dd
Pekarski <kiryl.pekarski.dev@gmail.com>
2025-09-06 20:55:13 +0300

    feat(backend): add authorization decorators to user controller

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 
+import { User as UserType } from "prisma/generated/client";
 import { Authorization } from "src/auth/decorators/authentication.decorator";
 import { User } from "src/auth/decorators/user.decorator";
 import { CreateUserDto } from "./dto/create-user.dto";
@@ -8,6 +9,12 @@ import { UserService } from './user.service';
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {};
+
+  @Authorization()
+  @Get("me")
+  async getCurrentUser(@User() user: UserType) {
+    return user;
+  }
 
   @Authorization()
   @HttpCode(HttpStatus.OK)
@@ -22,17 +29,26 @@ export class UserController {
     return this.userService.findById(id);
   }
   
+  @Authorization("admin")
   @Get("email/:email")
   async findByEmail(@Param("email") email: string) {
     return this.userService.findByEmail(email);
   }
-  
+
+  @Authorization("admin")
   @Post()
   async create(@Body() dto: CreateUserDto) {
     return this.userService.create(dto);
   }
 
-  @Post(":id")
+  @Authorization()
+  @Delete("me")
+  async deleteCurrentUser(@User("id") id: string) {
+    return this.userService.delete(id);
+  }
+
+  @Authorization("admin")
+  @Delete(":id")
   async delete(@Param("id") id: string) {
     return this.userService.delete(id);
   }
